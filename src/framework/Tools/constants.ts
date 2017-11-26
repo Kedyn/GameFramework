3aef51b0e8101c932a2350b24f316abd8e4b6ba1
Kedyn Macedonio <mkedyn@yahoo.com>
2017-11-26 01:58:12 -0800

    Changed structure.

---
+++ src/framework/Tools/constants.ts
@@ -0,0 +1,17 @@
+export const VERSION: string = "0.0.2";
+
+export const SECOND: number = 1000;
+
+export enum INPUT_TYPE {
+    KEYDOWN,
+    KEYUP,
+    MOUSE_MOVE,
+    MOUSE_DOWN,
+    MOUSE_UP
+}
+
+export enum MOUSE_BUTTON {
+    LEFT,
+    MIDDLE,
+    RIGHT
+}
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
-import {Game} from "./framework/Game";
-import {TestScene} from "./test/TestScene";
+import { SimpleGame } from "./framework/Game";
+import { SimpleSceneManager } from "./framework/SceneManager";
+//import your scene
 
 window.onload = function () {
-    let canvas: HTMLCanvasElement = <HTMLCanvasElement>document.getElementById('game'); //gets canvas element
-    let game = new Game("Snake", canvas); //creates a game instance
-    game.pushScene(new TestScene(game)); //adds the initial scene
-    game.init(); //initializes the game
-};
+    SimpleGame.create();
+    //SimpleSceneManager.pushScene(); add your scene
+    SimpleGame.init();
+}
